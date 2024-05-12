eb7b94011b7d8e90ad85b5b4432b76b8a861e2f1
Simon Steinmann <simonste3@gmail.com>
2024-05-12 13:05:54 +0200

    use app_links

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,66 @@
+import 'dart:async';
+
 import 'package:teambalancer/common/localization.dart';
 import 'package:flutter/material.dart';
 import 'package:teambalancer/screens/main_screen.dart';
+import 'package:app_links/app_links.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => MainAppState();
+}
+
+class MainAppState extends State<MainApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _linkSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initDeepLinks();
+  }
+
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+
+    super.dispose();
+  }
+
+  Future<void> initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // Handle links
+    _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
+      openAppLink(uri);
+    });
+  }
+
+  void openAppLink(Uri uri) {
+    _navigatorKey.currentState?.pushNamed(uri.fragment);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: MainScreen("A4GH49"),
+      navigatorKey: _navigatorKey,
+      onGenerateRoute: (RouteSettings settings) {
+        final routeName = settings.name;
+        return MaterialPageRoute(
+          builder: (context) => MainScreen(routeName ?? ""),
+          settings: settings,
+        );
+      },
     );
   }
 }
